7b8850321a7ca8fb5d53fb44a5e50b516e5a2890
Marty Nelson <marty@martypdx.com>
2022-05-31 15:01:14 -0700

    working name tag!

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
 
 // set event listeners
+// get info from user input
+// use user input to update state
+// update DOM to reflect the new state
 nameInput.addEventListener('input', () => {
     const name = nameInput.value;
-
+    nameDisplay.textContent = name;
 });
 
-// syntax/spelling
-// wrong event name
-// is our function being called?
-
-
-// get info from user input
-    // use user input to update state
-    // update DOM to reflect the new state
