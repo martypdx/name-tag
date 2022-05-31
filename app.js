bb7cf2a38aa9267a376a22ee8e09fe7e7eb9a1b8
Marty Nelson <marty@martypdx.com>
2022-05-31 14:54:17 -0700

    validated event subscription

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 
 const nameInput = document.getElementById('name-input');
 
-// syntax/spelling
-// wrong element
-// bad id
 
-// what if I want to keep using that input???
 
 // set event listeners
-    // get info from user input
+nameInput.addEventListener('input', () => {
+    console.log('input happened!');
+});
+
+// syntax/spelling
+// wrong event name
+// is our function being called?
+
+
+// get info from user input
     // use user input to update state
     // update DOM to reflect the new state
